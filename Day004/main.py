186f4f05021b63d7c7f4123f663c86552f025b38
chaitanyakrishnakumar <findartofkumar@gmail.com>
2025-04-17 20:22:48 +0530

    Update main.py

--- Day004/main.py
+++ Day004/main.py
@@ -27,24 +27,31 @@ scissors = '''
 ---.__(___)
 '''
 
+other = '''
+    _______
+---'   ____)
+          __)_____
+       ____________)
+      (____)
+---.__(___)
+'''
 game_pics = [rock, paper, scissors]
 player_choice = int(input("🗣️wanna play 🪨📄✂️? Type 0 for Rock, 1 for Paper or 2 for Scissors: "))
+computer_choice = random.randint(0, 2)
 if 0 <= player_choice <= 2:
- print(game_pics[player_choice])
-
-computer_choice = random.randint(0,2)
-print(f"computer chose: ")
-print(game_pics[computer_choice])
+    print(game_pics[player_choice])
+    print(f"computer chose: ")
+    print(game_pics[computer_choice])
+    if player_choice == computer_choice:
+        print("it's a draw")
+    elif player_choice == 0 and computer_choice == 2:
+        print("You win!")
+    elif player_choice == 2 and computer_choice == 0:
+        print("You lose")
+    elif player_choice > computer_choice:
+        print("You win!")
+    elif player_choice < computer_choice:
+        print("You lose")
 
 if player_choice >= 3 or player_choice < 0:
-    print("You type invalid number, you lose.")
-elif player_choice == computer_choice:
-    print("it's a draw")
-elif player_choice == 0 and computer_choice == 2:
-    print("You win!")
-elif player_choice == 2 and computer_choice == 0:
-    print("You lose")
-elif player_choice > computer_choice:
-    print("You win!")
-elif player_choice < computer_choice:
-    print("You lose")
+    print(other)
