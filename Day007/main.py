ad413044a92d303a73eaca1b5ec6e3e0d0ccbf59
chaitanyakrishnakumar <findartofkumar@gmail.com>
2025-04-18 11:57:33 +0530

    Update main.py

--- Day007/main.py
+++ Day007/main.py
@@ -13,15 +13,16 @@ placeholder = ""
 word_length = len(chosen_word)
 for position in range(word_length):
     placeholder += "_"
-print("Word to guess: " + placeholder)
+print(placeholder)
 
 game_over = False
 correct_letters = []
 
 while not game_over:
 
-    print(f"****************************{lives}/6 LIVES LEFT****************************")
+    print(f"*********{lives}/6 LIVES LEFT*********")
     guess = input("Guess a letter: ").lower()
+    print("\n" * 10)
 
     if guess in correct_letters:
         print(f"You've already guessed {guess}")
@@ -41,15 +42,14 @@ while not game_over:
 
     if guess not in chosen_word:
         lives -= 1
-        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        print(f"'{guess}' is not in the word. You lose a life.")
 
         if lives == 0:
             game_over = True
-
-            print(f"***********************IT WAS {chosen_word}! YOU LOSE**********************")
+            print(f"*********IT WAS '{chosen_word}'. YOU LOSE*********")
 
     if "_" not in display:
         game_over = True
-        print("****************************YOU WIN****************************")
+        print("*********YOU WIN*********")
 
     print(stages[lives])
