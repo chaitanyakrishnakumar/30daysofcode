89bab3a9f659a13ca9afc7070373a1b99324c83f
chaitanyakrishnakumar <findartofkumar@gmail.com>
2025-04-19 15:11:05 +0530

    Create main.py

---
+++ Day009/main.py
@@ -0,0 +1,30 @@
+bids = {}
+
+def auction():
+    name = input("What is your name?: ")
+    bid = int(input("What's your bid in coins?: "))
+    bids[name] = bid
+
+
+print("welcome to the Auction for Rama's Heirloom Ring")
+
+auction()
+
+more_guys = True
+
+while more_guys:
+    q = input("Are there any other bidders? Type 'yes or 'no': ").lower()
+    if q == "yes":
+        print("\n" * 100)
+        auction()
+    elif q == "no":
+        more_guys = False
+        highest_bid = 0
+        for key in bids:
+            if bids[key] > highest_bid:
+                highest_bid = bids[key]
+
+        for key in bids:
+            if bids[key] == highest_bid:
+                winner = key
+                print(f"The winner is {winner} with a bid of {highest_bid} coins")
