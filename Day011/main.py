4aa62a98970132b89a9cd05441e19d6b23ceb169
chaitanyakrishnakumar <findartofkumar@gmail.com>
2025-04-22 17:08:08 +0530

    Update main.py

--- Day011/main.py
+++ Day011/main.py
@@ -2,13 +2,11 @@ import random
 from art import logo
 
 def deal_card():
-    """Returns a random card from the deck"""
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     card = random.choice(cards)
     return card
 
 def calculate_score(cards):
-    """Take a list of cards and return the score calculated from the cards"""
     if sum(cards) == 21 and len(cards) == 2:
         return 0
 
@@ -19,7 +17,6 @@ def calculate_score(cards):
     return sum(cards)
 
 def compare(u_score, c_score):
-    """Compares the user score u_score against the computer score c_score."""
     if u_score == c_score:
         return "Draw 🙃"
     elif c_score == 0:
