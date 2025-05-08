551c4e33f418ee872f55fcc3bc8748ea0468dfd5
chaitanyakrishnakumar <findartofkumar@gmail.com>
2025-05-08 13:07:20 +0530

    Create data.py

---
+++ Day017/data.py
@@ -0,0 +1,7 @@
+question_data = [
+    {"text": "A slug's blood is green.", "answer": "True"},
+    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
+    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
+    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+  ]
